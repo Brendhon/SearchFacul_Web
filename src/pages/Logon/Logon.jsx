bab484e8be6df6e417e9b9f8fac31541a4e63259
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-23 17:38:27 -0300

    add: Logon page

---
+++ src/pages/Logon/Logon.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Logon.css'
+
+import Header from '../../components/Header'
+
+const Logon = _ =>
+    <div>
+        <Header />
+        <h1>Hello Again</h1>
+    </div>
+
+
+export default Logon
