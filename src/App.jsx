3d53dc8c7bfc5d06ebc6cf759780d36cfde9bf40
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-23 16:41:43 -0300

    add: CSS in APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from './components/Header'
+import './App.css'
 
 function App() {
   return (
-    <h1>hello</h1>
+    <div className="main">
+      <Header />
+      <h1>Hello</h1>
+    </div>
   )
 }
 
