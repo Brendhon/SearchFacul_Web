05e6525e092645af9471a461626636933fe9835a
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-24 17:01:25 -0300

    add: Logout in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom'
+import { FiPower } from 'react-icons/fi'
 import books from '../../assets/img/books.svg'
 
-const Header = _ =>
+const Header = props =>
     <header className="header">
+
 
         <div className="logo">
             <img className="mr-3" src={books} alt="Livros"></img>
             <Link to="/" className="home" >Home</Link>
         </div>
 
-        <div className="content">
-            <strong className="btn btn-primary mr-3">Login</strong>
-            <Link to="/register" className="btn btn-secondary">Cadastrar</Link>
-        </div>
+        {!props.authenticated ?
+            <div className="content">
+                <strong className="btn btn-primary mr-3">Login</strong>
+                <Link to="/register" className="btn btn-secondary">Cadastrar</Link>
+            </div> :
+            <div className="content">
+                <Link to="/" className="btn btn-outline-danger logout"><FiPower /></Link>
+            </div>
+        }
+
 
     </header>
 
