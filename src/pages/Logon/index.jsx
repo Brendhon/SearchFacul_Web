6074cc60a2cb0ff7481da9f08f33301c9dd576ec
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-24 01:35:53 -0300

    refact: Register

--- src/pages/Logon/index.jsx
+++ src/pages/Logon/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './styles.css'
 
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
 import logo from '../../assets/img/logo2.svg'
 
 const Logon = _ =>
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,44 +1,14 @@
 import React from 'react'
 import './styles.css'
 
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+import FormUniversity from '../../components/Form/FormUniversity'
 
 const Register = _ =>
     <div className='register-container'>
-
         <Header />
-
-        <div className='register-content'>
-
-            <h1>Cadastre sua Universidade</h1>
-
-            <section className="form">
-
-                <form>
-
-                    <input className="IES" className="IES" placeholder="IES" />
-                    <input className="telephone" placeholder="Telefone" type="tel" />
-                    <input className="email" placeholder="Email" type="email" />
-                    <input className="UF" placeholder="UF" />
-                    <input className="password" placeholder="Senha" />
-                    <input className="city" placeholder="Cidade" />
-                    <input className="confirmPassword" placeholder="Confirme sua senha" />
-                    <input className="address" placeholder="Endereço" />
-                    <input className="site" placeholder="Site" type="url" />
-
-                    <div className="radio">
-                        <input className="private" type="radio" id="Privada" name="gender" />Pública
-                        <input className="public" type="radio" id="Publica" name="gender" />Privada
-                    </div>
-
-
-                    <button className="button" type="submit">Entrar</button>
-
-                </form>
-
-            </section>
-        </div>
+        <FormUniversity title="Cadastre sua Universidade" />
         <Footer />
     </div>
 
