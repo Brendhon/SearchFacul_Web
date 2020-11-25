88563c75a146de24e8aab95f47000ba5f30ba9d8
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-24 22:57:57 -0300

    change: CSS classes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,39 +5,25 @@ const Card = _ =>
 
     <div className="card-container">
 
-        <strong className="ies">Instituto Nacional de Telecomunicações</strong>
+        <strong className="card-ies">Instituto Nacional de Telecomunicações</strong>
 
-        <div className="body">
+        <div className="card-content">
 
-            <h5 className="title"> Engenharia de computação</h5>
-            <p className="text">Some quick example text build on the card title and make up the bulk of the card's content.</p>
+            <h5 className="card-content-title"> Engenharia de computação</h5>
+            <p className="card-content-text">Some quick example text build on the card title and make up the bulk of the card's content.</p>
 
         </div>
 
-        <div className="list">
+        <div className="card-list-data">
 
-            <div className="list-item">
-
-                <div className="list-data">
-                    <p><strong>Titulação</strong>: Bacharelado</p>
-                </div>
-
-                <div className="list-data">
-                    <p><strong>Modalidade</strong>: Online</p>
-                </div>
-
+            <div className="card-list-item">
+                <p><strong>Titulação</strong>: Bacharelado</p>
+                <p><strong>Modalidade</strong>: Online</p>
             </div>
 
-            <div className="list-item">
-
-                <div className="list-data">
-                    <p><strong>Duração</strong>: 5 anos</p>
-                </div>
-
-                <div className="list-data">
-                    <p><strong>Nota MEC</strong>: 5</p>
-                </div>
-
+            <div className="card-list-item">
+                <p><strong>Duração</strong>: 5 anos</p>
+                <p><strong>Nota MEC</strong>: 5</p>
             </div>
 
         </div>
