7d91666a842d1094cc9febe6148d7c27deae0a3d
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-24 23:14:19 -0300

    change: name of CSS classes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import './Footer.css'
 import React from 'react'
 
 const Footer = _ =>
-    <footer className="footer">
+    <footer className="footer-container">
         <span>Made by Brendhon Moreira</span>
     </footer>
 
