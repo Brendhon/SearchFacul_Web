2542792c74d54454b01e04909c941eeaadd5b37f
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-26 15:40:45 -0300

    create: Alert component

---
+++ src/components/Alert/Alert.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Alert from '@material-ui/lab/Alert'
+
+// eslint-disable-next-line
+export default props => {
+
+    const handleClose = _ => props.onClose()
+
+    const CName = props.open ? "alert-absolute" : "alert-none"
+
+    return (
+        <div className={CName}>
+            <Alert severity={props.type} onClose={handleClose}>{props.text}</Alert>
+        </div>
+    )
+}
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import './Card.css'
 
 import { FiTrash2, FiEdit } from 'react-icons/fi'
-import Alert from '@material-ui/lab/Alert'
 
+import Alert from  '../Alert/Alert'
 import Modal from '../Modal/Modal'
 
 import CourseForm from '../Form/Course'
@@ -12,13 +12,13 @@ import CardInfo from '../CardInfo/CardInfo'
 const Card = props => {
 
     // Declaração de estados
-    const [visible, setVisible] = useState("alert-none")
+    const [alert, setAlert] = useState(false)
     const [edit, setEdit] = useState(false)
     const [info, setInfo] = useState(false)
 
     // Declaração de funções
-    const visibleTrue = _ => setVisible("alert-absolute")
-    const visibleFalse = _ => setVisible("alert-none")
+    const alertOpen = _ => setAlert(true)
+    const alertClose = _ => setAlert(false)
 
     const editOpen = _ => setEdit(true)
     const editClose = _ => setEdit(false)
@@ -40,7 +40,7 @@ const Card = props => {
                             <FiEdit />
                         </button>
 
-                        <button className="card-top-button" onClick={visibleTrue}>
+                        <button className="card-top-button" onClick={alertOpen}>
                             <FiTrash2 />
                         </button>
 
@@ -69,9 +69,7 @@ const Card = props => {
             </footer>
 
             {/* Componentes com posições não fixadas */}
-            <div className={visible}>
-                <Alert onClose={visibleFalse}>Deletado com sucesso</Alert>
-            </div>
+            <Alert type="info" text="Deletado com sucesso" open={alert} onClose={alertClose}/>
 
             <Modal open={edit} onClose={editClose}>
                 <CourseForm title="Digite os novos dados" />
