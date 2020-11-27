415409a1ac4443e52273bf4de848e54969225aac
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-26 21:46:09 -0300

    create: Search page

---
+++ src/pages/Search/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles.css'
+
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+
+const Search = _ =>
+    <div className='search-container'>
+
+        <Header />
+
+            <div className="search-content">
+                <h1>Hello</h1>
+            </div>
+
+        <Footer />
+
+    </div>
+
+export default Search
--- src/routes.jsx
+++ src/routes.jsx
@@ -5,13 +5,15 @@ import Logon from './pages/Logon'
 import Register from './pages/Register'
 import CreateCourse from './pages/CreateCourse'
 import Profile from './pages/Profile'
+import Search from './pages/Search'
 
 const Routes = _ =>
 
     <BrowserRouter>
 
         <Switch>
-            <Route path='/' exact component={Logon} />
+            <Route path='/' exact component={Search} />
+            <Route path='/logon' exact component={Logon} />
             <Route path='/register' component={Register} />
             <Route path='/profile' component={Profile} />
             <Route path='/course/create' component={CreateCourse} />
