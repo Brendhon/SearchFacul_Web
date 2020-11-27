904d00d5bc5f2339e7d00620d141c1296b595d18
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-27 16:13:36 -0300

    add: CSS global

--- src/pages/Logon/index.jsx
+++ src/pages/Logon/index.jsx
@@ -6,22 +6,31 @@ import Footer from '../../components/Footer/Footer'
 import logo from '../../assets/img/logo2.svg'
 
 const Logon = _ =>
-    <div className='logon-container'>
+    <div className='container'>
+
         <Header />
-        <div className='logon-content'>
+
+        <div className='content-row logon-content'>
 
             <section className="logon-form">
+
                 <form>
+
                     <h1>Faça seu Logon</h1>
                     <input className="mb-2" placeholder="Email" type="email" />
                     <input placeholder="Senha" type="password" />
                     <button className="button" type="submit">Entrar</button>
+
                 </form>
+
             </section>
 
             <img src={logo} alt="logo"></img>
+
         </div>
+
         <Footer/>
+        
     </div>
 
 
