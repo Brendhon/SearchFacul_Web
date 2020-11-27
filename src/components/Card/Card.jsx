1b246d4674bfece9059ce4625383e3e160373eaf
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-27 17:15:39 -0300

    change: CSS

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,7 @@ import './Card.css'
 
 import { FiTrash2, FiEdit } from 'react-icons/fi'
 
-import Alert from  '../Alert/Alert'
+import Alert from '../Alert/Alert'
 import Modal from '../Modal/Modal'
 
 import CourseForm from '../Form/Course'
@@ -56,8 +56,12 @@ const Card = props => {
             {/* Conteúdo do Cartão */}
             <div className="card-content">
 
-                <h5 className="card-content-title"> Engenharia de computação</h5>
-                <p className="card-content-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque justo nulla, volutpat in sollicitudin vitae, sollicitudin eget risus. Nulla facilisi. Suspendisse congue, quam eu pharetra convallis, purus lacus malesuada purus, vitae blandit turpis nunc ac nisi. Suspendisse vitae iaculis orci, sed venenatis odio. Integer eget dui et felis ullamcorper laoreet. Integer egestas aliquam egestas. Etiam vitae eleifend metus. Nulla porta lorem eu diam lacinia, nec sollicitudin tellus sodales. Donec eros risus, ultrices eget urna in, gravida semper urna. Pellentesque eleifend mi at felis tempus rutrum. Nullam nulla felis, semper quis convallis elementum, finibus fringilla libero. Nunc sodales, massa id fermentum interdum, nibh tellus aliquam ligula, vel maximus lectus turpis vitae metus.</p>
+                <div className="card-content-title">
+                    <h5> Engenharia de computação </h5>
+                </div>
+                <div className="card-content-text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque justo nulla, volutpat in sollicitudin vitae, sollicitudin eget risus. Nulla facilisi. Suspendisse congue, quam eu pharetra convallis, purus lacus malesuada purus, vitae blandit turpis nunc ac nisi. Suspendisse vitae iaculis orci, sed venenatis odio. Integer eget dui et felis ullamcorper laoreet. Integer egestas aliquam egestas. Etiam vitae eleifend metus. Nulla porta lorem eu diam lacinia, nec sollicitudin tellus sodales. Donec eros risus, ultrices eget urna in, gravida semper urna. Pellentesque eleifend mi at felis tempus rutrum. Nullam nulla felis, semper quis convallis elementum, finibus fringilla libero. Nunc sodales, massa id fermentum interdum, nibh tellus aliquam ligula, vel maximus lectus turpis vitae metus.</p>
+                </div>
 
             </div>
 
@@ -69,7 +73,7 @@ const Card = props => {
             </footer>
 
             {/* Componentes com posições não fixadas */}
-            <Alert type="info" text="Deletado com sucesso" open={alert} onClose={alertClose}/>
+            <Alert type="info" text="Deletado com sucesso" open={alert} onClose={alertClose} />
 
             <Modal open={edit} onClose={editClose}>
                 <CourseForm title="Digite os novos dados" />
