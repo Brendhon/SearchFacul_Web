44692a53d6ce122272b8757dbff0a425e7120613
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-27 18:31:53 -0300

    change: CSS

--- src/components/CardInfo/CardInfo.jsx
+++ src/components/CardInfo/CardInfo.jsx
@@ -23,19 +23,17 @@ const CardInfo = props =>
 
             <div className="info-content-subtitle">
 
-                <p className="info-content-data-titration" >Bacharelado</p>
+                <p>Bacharelado</p>
 
-                <p className="info-content-data-modality">Presencial</p>
+                <p>Presencial</p>
 
-                <p className="info-content-data-duration"><strong className="info-content-data-icon"><FiClock />
-                </strong>5 anos</p>
+                <p className="align-items"><FiClock className="info-content-data-icon" />5 anos</p>
 
-                <p className="info-content-data-score">5 <strong className="info-content-data-icon"><FaStar />
-                </strong></p>
+                <p className="align-items">5 <FaStar className="info-content-data-icon" /></p>
 
             </div>
 
-            <p className="info-content-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque justo nulla, volutpat in sollicitudin vitae, sollicitudin eget risus. Nulla facilisi. Suspendisse congue, quam eu pharetra convallis, purus lacus malesuada purus, vitae blandit turpis nunc ac nisi. Suspendisse vitae iaculis orci, sed venenatis odio. Integer eget dui et felis ullamcorper laoreet. Integer egestas aliquam egestas. Etiam vitae eleifend metus. Nulla porta lorem eu diam lacinia, nec sollicitudin tellus sodales. Donec eros risus, ultrices eget urna in, gravida semper urna. Pellentesque eleifend mi at felis tempus rutrum. Nullam nulla felis, semper quis convallis elementum, finibus fringilla libero. Nunc sodales, massa id fermentum interdum, nibh tellus aliquam ligula, vel maximus lectus turpis vitae metus.</p>
+            <p className="info-content-description">saddddddddsdadsasd asdmaskdma asdpasmdpasd dmamdapsmda adadpaomsdoas asdma´smdasmd asdasdoasdasdkams adksad asdaskdmas aoksdoasdk asdokpasdkam aksdaosd adkoaskd asdkpasdk asdokapsodk asdokpasdkpa aspdo odapsdka adokpadk adwokapwd wadoapdka dokapd adkaopwd awdokopawkda ddowkaopd awdokapwoodk awdokawp asjdoiasn asodkpaoskd asdpkoaoskd asodkpasodk askdopaskd asodkpasokd asokdpaoskdp sadkapdka asdokap asdasdas asdasd asdasd asdadsijasasdijoasd asdias</p>
 
             <div className="info-content-data">
 
@@ -43,8 +41,8 @@ const CardInfo = props =>
 
                     <legend>Contato</legend>
 
-                    <p><strong className="info-content-data-icon"><FiMail /></strong>guilherme@gmail.lo.br</p>
-                    <p><strong className="info-content-data-icon"><FiPhone /></strong>34546109</p>
+                    <p className="align-items"><FiMail className="info-content-data-icon" /> guilherme@gmail.lo.br</p>
+                    <p className="align-items"><FiPhone className="info-content-data-icon" /> 34546109</p>
 
                 </fieldset>
 
@@ -52,8 +50,8 @@ const CardInfo = props =>
 
                     <legend>Localização</legend>
 
-                    <p><strong className="info-content-data-icon"><FiMap /></strong>Santa Rita - MG</p>
-                    <p><strong className="info-content-data-icon"><FiMapPin /></strong>Avenida Inatel, 12</p>
+                    <p className="align-items"><FiMap className="info-content-data-icon" />Santa Rita - MG</p>
+                    <p className="align-items"><FiMapPin className="info-content-data-icon" />Avenida Inatel, 12</p>
 
                 </fieldset>
 
@@ -65,7 +63,7 @@ const CardInfo = props =>
         <footer className="info-bottom">
 
             {/* eslint-disable-next-line */}
-            <a target="_blank" href="https://inatel.br/home/">Entre no site para saber mais!</a>
+            <a className="a-link" style={{ fontSize: 17, fontWeight: 600 }} target="_blank" href="https://inatel.br/home/">Entre no site para saber mais</a>
 
         </footer>
 
