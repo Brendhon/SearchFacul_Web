5faee6ac5dea26f93f8e96058db737877cf17f56
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-29 18:46:32 -0300

    fix

--- src/components/Form/University.jsx
+++ src/components/Form/University.jsx
@@ -26,80 +26,76 @@ const FormUniversity = props => {
             validationSchema={universitySchema}
             onSubmit={props.onSubmit}>
 
-            {({ values }) => (
-                <section className="university-content">
+            <section className="university-content">
 
-                    {props.title ? <h1>{props.title}</h1> : <div />}
+                {props.title ? <h1>{props.title}</h1> : <div />}
 
-                    <Form className="university-form">
+                <Form className="university-form">
 
-                        <Field className="form-field IES"
-                            name="IES"
-                            placeholder="IES" />
-                        <ErrorMessage className="form-error error-ies" component="span" name="IES" />
+                    <Field className="form-field IES"
+                        name="IES"
+                        placeholder="IES" />
+                    <ErrorMessage className="form-error error-ies" component="span" name="IES" />
 
-                        <Field className="form-field email"
-                            name="email"
-                            placeholder="Email"
-                            type="email" />
-                        <ErrorMessage className="form-error error-email" component="span" name="email" />
+                    <Field className="form-field email"
+                        name="email"
+                        placeholder="Email"
+                        type="email" />
+                    <ErrorMessage className="form-error error-email" component="span" name="email" />
 
-                        <Field className="form-field password"
-                            name="password"
-                            placeholder="Senha"
-                            type={props.token ? 'text' : 'password'} />
-                        <ErrorMessage className="form-error error-password" component="span" name="password" />
+                    <Field className="form-field password"
+                        name="password"
+                        placeholder="Senha"
+                        type={props.token ? 'text' : 'password'} />
+                    <ErrorMessage className="form-error error-password" component="span" name="password" />
 
-                        <Field className="form-field confirmPassword"
-                            name="confirmPassword"
-                            placeholder="Confirme sua senha"
-                            type={props.token ? 'text' : 'password'} />
-                        <ErrorMessage className="form-error error-confirmPassword" component="span" name="confirmPassword" />
+                    <Field className="form-field confirmPassword"
+                        name="confirmPassword"
+                        placeholder="Confirme sua senha"
+                        type={props.token ? 'text' : 'password'} />
+                    <ErrorMessage className="form-error error-confirmPassword" component="span" name="confirmPassword" />
 
-                        <Field className="form-field city"
-                            name="city"
-                            placeholder="Cidade" />
-                        <ErrorMessage className="form-error error-city" component="span" name="city" />
+                    <Field className="form-field city"
+                        name="city"
+                        placeholder="Cidade" />
+                    <ErrorMessage className="form-error error-city" component="span" name="city" />
 
-                        <Field className="form-field telephone"
-                            name="telephone"
-                            placeholder="Telefone" />
-                        <ErrorMessage className="form-error error-telephone" component="span" name="telephone" />
+                    <Field className="form-field telephone"
+                        name="telephone"
+                        placeholder="Telefone" />
+                    <ErrorMessage className="form-error error-telephone" component="span" name="telephone" />
 
-                        <Field className="form-field address"
-                            name="address"
-                            placeholder="Endereço" />
-                        <ErrorMessage className="form-error error-address" component="span" name="address" />
+                    <Field className="form-field address"
+                        name="address"
+                        placeholder="Endereço" />
+                    <ErrorMessage className="form-error error-address" component="span" name="address" />
 
-                        <Field className="form-field UF"
-                            name="uf"
-                            placeholder="UF"
-                            maxLength="2" />
-                        <ErrorMessage className="form-error error-uf" component="span" name="uf" />
+                    <Field className="form-field UF"
+                        name="uf"
+                        placeholder="UF"
+                        maxLength="2" />
+                    <ErrorMessage className="form-error error-uf" component="span" name="uf" />
 
-                        <Field className="form-field site"
-                            name="site"
-                            placeholder="Site" />
-                        <ErrorMessage className="form-error error-site" component="span" name="site" />
+                    <Field className="form-field site"
+                        name="site"
+                        placeholder="Site" />
+                    <ErrorMessage className="form-error error-site" component="span" name="site" />
 
-                        <Field as="select"
-                            className="form-field select-category"
-                            name="category">
+                    <Field as="select"
+                        className="form-field select-category"
+                        name="category">
 
-                            <option value="">Selecione uma categoria</option>
-                            <option value="private">Privado</option>
-                            <option value="public">Pública</option>
-                        </Field>
-                        <ErrorMessage className="form-error error-category" component="span" name="category" />
+                        <option value="">Selecione uma categoria</option>
+                        <option value="private">Privado</option>
+                        <option value="public">Pública</option>
+                    </Field>
+                    <ErrorMessage className="form-error error-category" component="span" name="category" />
 
-                        <button className="button" type="submit">Enviar</button>
+                    <button className="button" type="submit">Enviar</button>
 
-                    </Form>
-                
-                </section>
-            )}
+                </Form>
 
-
+            </section>
 
         </Formik>
     )
