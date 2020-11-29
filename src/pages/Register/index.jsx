62844decf936b20162150deb2347301c2bc4671c
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-29 19:35:34 -0300

    add: connection with database

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,21 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css'
 
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import UniversityForm from '../../components/Form/University'
+import Alert from '../../components/Alert/Alert'
 
-const Register = _ =>
-    <div className='container'>
+import { getError, removeEmptyData } from '../../utils/utils'
 
-        <Header />
+import api from '../../services/api'
 
-        <div className="content-column box register-content">
-            <UniversityForm title="Cadastre sua Universidade" />
-        </div>
+const Register = _ => {
 
-        <Footer />
+    // Setando os estados do alert
+    const [open, setOpen] = useState(false)
+    const [message, setMessage] = useState("")
+    const [type, setType] = useState("info")
 
-    </div>
+    // Setando funções do Alert
+    const alertDisabled = _ => setOpen(false)
+    const errorAlertEnabled = message => {
+        setMessage(message)
+        setType("error")
+        setOpen(true)
+    }
+    const successAlertEnabled = message => {
+        setMessage(message)
+        setType("success")
+        setOpen(true)
+    }
 
+    const handleRegister = async data => {
+
+        //Removendo campos desnecessários
+        delete data["confirmPassword"]
+        removeEmptyData(data)
+
+        try {
+            await api.post('/university', data)
+            successAlertEnabled('Cadastro realizado com sucesso') // Mostrando um alert para o usuário de sucesso
+        } catch (error) {
+            errorAlertEnabled(getError(error)) // Mostrando um alert para o usuário de fracasso
+        }
+    }
+
+    return (
+        <div className='container'>
+
+            <Header />
+
+            <div className="content-column box register-content">
+                <UniversityForm title="Cadastre sua Universidade" onSubmit={handleRegister} />
+            </div>
+
+            <Footer />
+
+            {/* Componentes com posições não fixadas */}
+            <Alert type={type} text={message} open={open} onClose={alertDisabled} />
+
+        </div>)
+}
 export default Register
