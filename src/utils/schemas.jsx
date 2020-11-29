2a5a7996661d649ec6b757223eaa9210ac78cd7b
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-29 15:02:21 -0300

    create: schema with Yup

---
+++ src/utils/schemas.jsx
@@ -0,0 +1,21 @@
+import * as yup from 'yup'
+
+const courseSchema = yup.object().shape({
+
+    name: yup.string().required('Informe um nome'),
+
+    duration: yup.string().trim().required('Informe a duração do curso'),
+
+    score: yup.string()
+        .length(1, 'Apenas um número')
+        .matches(/[0-5]+/gi, "Apenas números de 0 - 5"),
+
+    description: yup.string().trim().required('Informe uma descrição'),
+
+    titration: yup.string().required('Escolha uma titulação'),
+
+    modality: yup.string().required('Escolha uma modalidade'),
+
+})
+
+export { courseSchema }
