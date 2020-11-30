4f8330fb5df2f8e8ac031a5a2c0dea31ea1d1594
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-30 14:26:13 -0300

    add: link to course update page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,25 +2,20 @@ import React, { useState } from 'react'
 import './Card.css'
 
 import { FiTrash2, FiEdit } from 'react-icons/fi'
+import { useHistory } from 'react-router-dom'
 
 import Modal from '../Modal/Modal'
-
-import CourseForm from '../Form/Course'
 import CardInfo from '../CardInfo/CardInfo'
-
-import Alert from '../Alert/Alert'
 
 const Card = props => {
 
     const [info, setInfo] = useState(false)
     const [hide, setHide] = useState(false)
-    const [edit, setEdit] = useState(false)
-
-    const editOpen = _ => setEdit(true)
-    const editClose = _ => setEdit(false)
 
     const infoOpen = _ => setInfo(true)
     const infoClose = _ => setInfo(false)
+
+    const history = useHistory() // Permite fazer a navegação por JS
 
     return (
         <div className={hide ? "card-container card-container-hide" : "card-container"}>
@@ -32,14 +27,24 @@ const Card = props => {
 
                     <div className="card-top-authenticated">
 
-                        <button className="card-top-button" onClick={editOpen}>
+                        <button className="card-top-button"
+                            onClick={_ => history.push(`/course/edit/${props.id}`, {
+                                id: props.id,
+                                name: props.name,
+                                description: props.description,
+                                duration: props.duration,
+                                titration: props.titration,
+                                modality: props.modality,
+                                score: props.score
+                            })}>
                             <FiEdit />
                         </button>
 
-                        <button className="card-top-button" onClick={() => {
-                            setHide(true)
-                            setTimeout(_ => props.handleDeleteCourse(props.id), 500)
-                        }}>
+                        <button className="card-top-button"
+                            onClick={() => {
+                                setHide(true)
+                                setTimeout(_ => props.handleDeleteCourse(props.id), 500)
+                            }}>
                             <FiTrash2 />
                         </button>
 
@@ -70,14 +75,6 @@ const Card = props => {
                 <button className="a-link" onClick={infoOpen}>Saiba mais</button>
 
             </footer>
-
-            <Modal open={edit} onClose={editClose}>
-                <CourseForm title="Digite os novos dados" {...props}
-                    onSubmit={(values) => {
-                        props.handleEditCourse(values, props.id)
-                    }} />
-                <Alert />
-            </Modal>
 
             <Modal open={info} onClose={infoClose}>
                 <CardInfo {...props} />
