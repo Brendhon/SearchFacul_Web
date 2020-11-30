596d311e0ea1db30b3112c3e9baf09bcb2be7bed
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-30 14:54:37 -0300

    add: transitions in card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,6 +7,8 @@ import { useHistory } from 'react-router-dom'
 import Modal from '../Modal/Modal'
 import CardInfo from '../CardInfo/CardInfo'
 
+import Fade from '@material-ui/core/Fade'
+
 const Card = props => {
 
     const [info, setInfo] = useState(false)
@@ -18,69 +20,71 @@ const Card = props => {
     const history = useHistory() // Permite fazer a navegação por JS
 
     return (
-        <div className={hide ? "card-container card-container-hide" : "card-container"}>
+        <Fade in={true} style={{ transitionDelay: '500ms' }}>
+            <div className={hide ? "card-container card-container-hide" : "card-container"}>
 
-            {/* Cabeçalho do Card */}
-            <header className="card-top">
+                {/* Cabeçalho do Card */}
+                <header className="card-top">
 
-                {!props.authenticated ? <div style={{ margin: 10 }} /> :
+                    {!props.authenticated ? <div style={{ margin: 10 }} /> :
 
-                    <div className="card-top-authenticated">
+                        <div className="card-top-authenticated">
 
-                        <button className="card-top-button"
-                            onClick={_ => history.push(`/course/update/${props.id}`, {
-                                id: props.id,
-                                name: props.name,
-                                description: props.description,
-                                duration: props.duration,
-                                titration: props.titration,
-                                modality: props.modality,
-                                score: props.score
-                            })}>
-                            <FiEdit />
-                        </button>
+                            <button className="card-top-button"
+                                onClick={_ => history.push(`/course/update/${props.id}`, {
+                                    id: props.id,
+                                    name: props.name,
+                                    description: props.description,
+                                    duration: props.duration,
+                                    titration: props.titration,
+                                    modality: props.modality,
+                                    score: props.score
+                                })}>
+                                <FiEdit />
+                            </button>
 
-                        <button className="card-top-button"
-                            onClick={() => {
-                                setHide(true)
-                                setTimeout(_ => props.handleDeleteCourse(props.id), 300)
-                            }}>
-                            <FiTrash2 />
-                        </button>
+                            <button className="card-top-button"
+                                onClick={() => {
+                                    setHide(true)
+                                    setTimeout(_ => props.handleDeleteCourse(props.id), 300)
+                                }}>
+                                <FiTrash2 />
+                            </button>
 
+                        </div>
+                    }
+
+                    <div className="card-ies">
+                        <strong >{props.IES}</strong>
                     </div>
-                }
 
-                <div className="card-ies">
-                    <strong >{props.IES}</strong>
+                </header>
+
+                {/* Conteúdo do Cartão */}
+                <div className="card-content">
+
+                    <div className="card-content-title">
+                        <h5> {props.name} </h5>
+                    </div>
+                    <div className="card-content-text">
+                        <p>{props.description}</p>
+                    </div>
+
                 </div>
 
-            </header>
+                {/* Rodapé do Cartão */}
+                <footer className="card-bottom">
 
-            {/* Conteúdo do Cartão */}
-            <div className="card-content">
+                    <button className="a-link" onClick={infoOpen}>Saiba mais</button>
 
-                <div className="card-content-title">
-                    <h5> {props.name} </h5>
-                </div>
-                <div className="card-content-text">
-                    <p>{props.description}</p>
-                </div>
+                </footer>
+
+                <Modal open={info} onClose={infoClose}>
+                    <CardInfo {...props} />
+                </Modal>
 
             </div>
-
-            {/* Rodapé do Cartão */}
-            <footer className="card-bottom">
-
-                <button className="a-link" onClick={infoOpen}>Saiba mais</button>
-
-            </footer>
-
-            <Modal open={info} onClose={infoClose}>
-                <CardInfo {...props} />
-            </Modal>
-
-        </div>
+        </Fade>
     )
 }
 
