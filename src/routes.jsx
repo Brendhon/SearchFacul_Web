fcd40adfe29cb83f1c55daa2ac4b8b2bc3dae292
Brendhon <brendhon.e.c.m@gmail.com>
2020-12-09 17:55:18 -0300

    create: Private route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 import Logon from './pages/Logon'
 import UniversityRegister from './pages/UniversityRegister'
@@ -10,19 +10,44 @@ import Profile from './pages/Profile'
 import Search from './pages/Search'
 import SearchResult from './pages/SearchResult'
 
+import { isAuthenticated } from '../src/utils/auth'
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    
+    <Route {...rest} render={props => (
+
+        // Verificando se ele esta autentificado: Sim - renderiza; Não - redireciona
+        isAuthenticated() ?
+            (
+                <Component {...props} />
+            ) :
+            (
+                <Redirect to={{ pathname: '/logon', state: { from: props.location } }} />
+            )
+
+    )} />
+)
+
 const Routes = _ =>
 
     <BrowserRouter>
 
         <Switch>
+
             <Route path='/' exact component={Search} />
             <Route path='/result' component={SearchResult} />
-            <Route path='/logon' exact component={Logon} />
-            <Route path='/profile' component={Profile} />
+            <Route path='/logon' component={Logon} />
+            <PrivateRoute path='/profile' component={Profile} />
             <Route path='/register' component={UniversityRegister} />
-            <Route path='/university/update' component={UniversityUpdate} />
-            <Route path='/course/create' component={CourseRegister} />
-            <Route path='/course/update/:id' component={CourseUpdate} />
+            <PrivateRoute path='/university/update' component={UniversityUpdate} />
+            <PrivateRoute path='/course/create' component={CourseRegister} />
+            <PrivateRoute path='/course/update/:id' component={CourseUpdate} />
+
+            {/* Redirecionando para a pagina principal caso não tenha a rota pesquisada */}
+            <Route path='*'>
+                <Redirect to="/" />
+            </Route>
+
         </Switch>
 
     </BrowserRouter>
